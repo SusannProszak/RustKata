31deb60889d69cebc95ba1f2815de1110aafa58b
Susann Proszak <s.proszak@gmail.com>
2016-07-07 21:30:03 +0200

    add test for prime factors of two

--- src/main.rs
+++ src/main.rs
@@ -6,3 +6,8 @@ fn prime_factors(num: i64) -> Vec<i64> {
 fn test_prime_factors_one() {
     assert_eq!(prime_factors(1), []);
 }
+
+#[test]
+fn test_prime_factors_two() {
+    assert_eq!(prime_factors(2), [2]);
+}
