88c579e500e0cb4e52703cd27adba54dc2bc168a
Susann Proszak <s.proszak@gmail.com>
2016-07-07 21:27:43 +0200

    add first test

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
-fn main() {
-    println!("Hello, world!");
+fn prime_factors(num: i64) -> Vec<i64> {
+    vec![]
+}
+
+#[test]
+fn test_prime_factors_one() {
+    assert_eq!(prime_factors(1), []);
 }
